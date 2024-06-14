fc706dc1ea9f1ddc6d81d302cdc8d315159d0157
spidermansinman <113697815+spidermansinman@users.noreply.github.com>
2024-06-14 16:52:42 +0200

    Configuración Script PlayerInfo para mostrar las pilas recogidas

--- Assets/02. Scripts/UI/PlayerInfo.cs
+++ Assets/02. Scripts/UI/PlayerInfo.cs
@@ -7,17 +7,37 @@ public class PlayerInfo : MonoBehaviour
     [Tooltip("The text object that shows the score of the player")]
     private TextMeshProUGUI _scoreText;
 
-    // To activate the UI, activate the UI game object and subscribe to the coin obtain
+    [SerializeField]
+    [Tooltip("The text object that shows the battery count of the player")]
+    private TextMeshProUGUI _batteryText;
+
+    // To activate the UI, activate the UI game object and subscribe to the coin and battery obtain
     public void Activate(GameObject playerObject)
     {
         gameObject.SetActive(true);
+
         CoinObtainer coinObtainer = playerObject.GetComponent<CoinObtainer>();
-        coinObtainer.OnCoinObtained += OnCoinObtained;
+        if (coinObtainer != null)
+        {
+            coinObtainer.OnCoinObtained += OnCoinObtained;
+        }
+
+        BatteryObtainer batteryObtainer = playerObject.GetComponent<BatteryObtainer>();
+        if (batteryObtainer != null)
+        {
+            batteryObtainer.OnBatteryObtained += OnBatteryObtained;
+        }
     }
 
     // When you obtain a coin, show how many coins you have now
     private void OnCoinObtained(int coins)
     {
         _scoreText.text = coins.ToString();
+    }
+
+    // When you obtain a battery, show how many batteries you have now
+    private void OnBatteryObtained(int batteries)
+    {
+        _batteryText.text = batteries.ToString();
     }
 }
